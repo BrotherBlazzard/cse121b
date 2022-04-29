821cb2e842ba9f2a0ad5a72098b2beadaf073824
Blazzard <blazzard@gmail.com>
2022-04-29 12:24:57 -0600

    asdf

--- scripts/date.js
+++ scripts/date.js
@@ -1,2 +1,7 @@
-document.querySelector('#year').textContent = new Date().getFullYear();
+const d = new Date();
+const enddate = new Date('06/02/2022');
+let diff = (enddate - d) / (1000 * 60 * 60 * 24);
+
+document.querySelector('#year').textContent = d.getFullYear();
 document.querySelector('#lastModified').textContent = document.lastModified;
+document.querySelector('#daysLeft').textContent = diff.toFixed(0);
