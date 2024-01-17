0ec8af0cc5f5831b9a47a818ab600533f4a33ec5
Blazzard <blazzard@gmail.com>
2024-01-16 19:19:03 -0700

    updated assignment

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -2,27 +2,35 @@
 
 /* Step 1 - Setup type tasks - no code required */
 
+
 /* Step 2 - Variables */
-
-
-
+const fullName = "Brother Blazzard";
+const currentYear = new Date().getFullYear(); // "2024"
+const profilePicture = "images/blazzard.png";
 
 /* Step 3 - Element Variables */
 
 
-
+const foodElement = document.querySelector('#food');
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 
 
-
-
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}. He is a online faculty member in the CSE department at BYU-Idaho.`);
 
 
 /* Step 5 - Array */
+let favfoods = ["Pizza", "Ice Cream", "Tacos", "Sushi", "Pasta", "Burgers", "Steak", "Chicken", "Salad", "Fruit"];
 
-
+foodElement.innerHTML = favfoods;
+let newfood = 'chocolate';
+favfoods.push(newfood);
+foodElement.innerHTML += `<br>${favfoods}`;
 
 
 
